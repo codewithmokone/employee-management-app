631224910f909c72a2d83eb42cf4199211475f5a
codewithmokone <codewithmokone@gmail.com>
2023-08-11 15:56:27 +0200

    updated

--- app.js
+++ app.js
@@ -3,24 +3,27 @@ const express = require('express');
 // Express app
 const app = express();
 
+// Register view engine
+app.set('view engine', 'ejs');
+
 // Listen for request
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>Hi there</p>')
-    res.sendFile('./pages/index.html', {root: __dirname})
+    res.render('index');
 })
 
-app.get('/about', (req, res) => {
-    res.sendFile('./pages/about.html', {root: __dirname})
+app.get('/employeeslist', (req, res) => {
+    res.render('employeeslist')
 })
 
-// Redirects
-app.get('/about-us', (req, res) => {
-    res.redirect('/about');
-})
+// // Redirects
+// app.get('/about-us', (req, res) => {
+//     res.redirect('/about');
+// })
 
 // 404 page
 app.use((req,res) => {
-    res.sendFile('./pages/404.html', {root: __dirname})
+    res.status(404).render('404')
 })
